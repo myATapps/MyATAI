f2ba5e5ccdf689f832a3cc73c5913e95f3f80ba1
myATapps <hafizsaqiali6@gmail.com>
2026-05-15 20:41:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class TestApp(App):
+    def build(self):
+        return Label(text="AT PRO AI - Success")
+
+if __name__ == '__main__':
+    TestApp().run()
